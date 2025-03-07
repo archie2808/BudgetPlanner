ebb46980de669628c023e9b133542bc0e5b66b18
archie2808 <archiegledhill2808@gmail.com>
2025-03-07 09:41:07 +0000

    implemented JWT

--- app/schemas.py
+++ app/schemas.py
@@ -8,3 +8,10 @@ class LoginRequest(BaseModel):
     username: str
     password: str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None 
+
